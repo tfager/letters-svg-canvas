163580126dfd7240d5f52389a7f46fe74baecc5c
Timo Fager <tfager@gmail.com>
2022-07-14 21:59:29 +0300

    Working version with boxed letters

--- src/tfager/letters_svg_canvas.clj
+++ src/tfager/letters_svg_canvas.clj
@@ -1,26 +1,28 @@
 (ns tfager.letters-svg-canvas
   (:require [dali.io :as io]
             [dali.layout.stack :as stack]
+            [dali.layout.align :as align]
    ))
 
-#_(def document
-  [:dali/page {:width 260 :height 160}
-   [:dali/stack
-    {:position [10 10], :direction :right, :anchor :bottom-left, :gap 2}
-    (map (fn [letter]
-           [:text {:font-family "Impact" :font-size 26 :stroke :black :fill :none} letter]
-           ["A" "B" "C" "Å" "Ä" "Ö"]
-           ))
-    ]])
+(def font-name "Zurich BlkEx BT, Expanded")
+
+(def letters (vec "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ01234567890.!,2222"))
+
+(defn boxed-letter [letter-str]
+  [:dali/align {:axis :center}
+    [:rect {:fill :none :stroke :black :stroke-width 0.01} :_ [32 32]]
+    [:text {:font-family font-name :font-size 26 :stroke :black :stroke-width 0.01 :fill :none} letter-str]])
 
 (def document
-  [:dali/page {:width 260 :height 160}
-   [:dali/stack
-    {:position [10 10], :direction :right, :anchor :bottom-left, :gap 2}
-     [:text {:font-family "Impact" :font-size 26 :stroke :black :fill :none} "A"]
-     [:text {:font-family "Impact" :font-size 26 :stroke :black :fill :none} "B"]
-    ]])
+  [:dali/page
+   (vec (concat
+     [:dali/stack
+      {:position [10 10], :direction :right, :anchor :bottom-left, :gap 5}]
+      (vec (map (comp boxed-letter str) letters))))
+     ])
 
 (defn -main [& args]
+  ;; TODO: Box width according to letter width
+  ;; TODO: Align to baseline, not center (cf. A and Ä)
   (io/render-svg document "hello-world.svg"))
 
