a6a304ea6d87d051fdd36c3265a765a8c4725fd2
Timo Fager <tfager@gmail.com>
2022-07-16 11:45:28 +0300

    Use :surround to get bounding box according to letter width.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # letters-svg-canvas
-Programmatically layout desired letters into SVG
+Programmatically layout desired letters into SVG using the [dali library](https://github.com/stathissideris/dali). Intended for laser cutting the letters. 
+
+## Usage
+
+`lein run output.svg`
--- src/tfager/letters_svg_canvas.clj
+++ src/tfager/letters_svg_canvas.clj
@@ -1,28 +1,53 @@
 (ns tfager.letters-svg-canvas
   (:require [dali.io :as io]
-            [dali.layout.stack :as stack]
-            [dali.layout.align :as align]
+            [dali.layout.stack]
+            [dali.layout.align]
+            [dali.layout.surround]
    ))
 
 (def font-name "Zurich BlkEx BT, Expanded")
+(def stroke-width 0.1)
 
 (def letters (vec "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ01234567890.!,2222"))
 
+(def next-id (atom 1))
+
 (defn boxed-letter [letter-str]
-  [:dali/align {:axis :center}
-    [:rect {:fill :none :stroke :black :stroke-width 0.01} :_ [32 32]]
-    [:text {:font-family font-name :font-size 26 :stroke :black :stroke-width 0.01 :fill :none} letter-str]])
+  (let [id-tag (str "letter_" (swap! next-id inc))]
+    [:dali/align {:axis :center}
+      [:text {:id id-tag
+              :font-family font-name
+              :font-size 26
+              :stroke :black
+              :stroke-width stroke-width
+              :fill :none}
+       letter-str]
+      [:dali/surround {:select [(keyword (str "#" id-tag))]
+                       :padding 2
+                       :attrs {:fill :none
+                               :stroke :black
+                               :stroke-width stroke-width
+                               :height 32}}]]))
 
 (def document
   [:dali/page
    (vec (concat
      [:dali/stack
-      {:position [10 10], :direction :right, :anchor :bottom-left, :gap 5}]
+      {:position [10 10], :direction :right, :anchor :bottom-left, :gap 2}]
       (vec (map (comp boxed-letter str) letters))))
      ])
 
+; Test wide, narrow and high letters
+(def testdoc
+  [:dali/page
+   (vec (concat
+          [:dali/stack
+           {:position [10 10], :direction :right, :anchor :bottom-left, :gap 0}]
+          (vec (map (comp boxed-letter str) "AIMÖ"))))])
+
 (defn -main [& args]
-  ;; TODO: Box width according to letter width
   ;; TODO: Align to baseline, not center (cf. A and Ä)
-  (io/render-svg document "hello-world.svg"))
+  (if (< (count args) 1)
+    (throw (Exception. "Output SVG file name required as first argument")))
+  (io/render-svg testdoc (first args)))
 
